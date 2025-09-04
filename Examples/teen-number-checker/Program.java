ce21e978ea74abe9d8010b26483249d235bc7ef0
jeftegoes <jefte.goes.dev@outlook.com>
2025-09-03 23:41:11 -0300

    Update content.

--- Examples/teen-number-checker/Program.java
+++ Examples/teen-number-checker/Program.java
@@ -1,4 +1,12 @@
 public class Program {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(13));
+    }
+
     public static boolean hasTeen(int firstNumber, int secondNumber, int thirdNumber) {
         if ((firstNumber >= 13 && firstNumber <= 19) || (secondNumber >= 13 && secondNumber <= 19)
                 || (thirdNumber >= 13 && thirdNumber <= 19)) {
@@ -12,13 +20,5 @@ public class Program {
             return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(hasTeen(9, 99, 19));
-        System.out.println(hasTeen(23, 15, 42));
-        System.out.println(hasTeen(22, 23, 34));
-        System.out.println(isTeen(9));
-        System.out.println(isTeen(13));
     }
 }
